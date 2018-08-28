d987000ac1aee06e49bef0ea5ec1ac6d42eb691e
Szymon Lipka <szymon.lipka@infakt.pl>
2018-08-28 09:51:10 +0200

    add require simple_nip

--- lib/common_numbers_rails.rb
+++ lib/common_numbers_rails.rb
@@ -1,6 +1,9 @@
 require 'active_model/validations/nip_validator'
 require 'active_model/validations/pesel_validator'
 require 'active_model/validations/regon_validator'
+require 'active_model/validations/simple_nip_validator'
+
+require 'common_numbers/polish/simple_nip'
 
 require 'active_support/i18n'
 
